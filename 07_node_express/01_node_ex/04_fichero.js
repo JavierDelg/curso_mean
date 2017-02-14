ffe05f91a2f669593a14ec92076ae84b17492db8
G03-EQ10\tarde <jdelg95@hotmail.com>
2017-02-14 17:10:39 +0100

    App ejemplo

--- 07_node_express/01_node_ex/04_fichero.js
+++ 07_node_express/01_node_ex/04_fichero.js
@@ -14,16 +14,7 @@ app.get("/reservas/:idCliente", (request, response) => {
     var idCliente = request.params.idCliente;
     response.send(JSON.stringify(clientes[parseInt(idCliente)]));
 });
-app.route('/book')
-    .get(function(req, res) {
-        res.send('Get a random book');
-    })
-    .post(function(req, res) {
-        res.send('Add a book');
-    })
-    .put(function(req, res) {
-        res.send('Update the book');
-    });
+
 
 
 app.get("/save/:fichero.:extension", (request, response) => {
